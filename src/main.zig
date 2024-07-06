c3967160fd0a1cc1ec19d21a4cf059a9231b1e6b
Sigge Eriksson <sigge.eriksson@gmail.com>
2024-07-06 20:22:00 +0200

    Some basic json parsing

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,9 @@
 const std = @import("std");
+const expect = std.testing.expect;
+const expectEqual = std.testing.expectEqual;
+const expectEqualStrings = std.testing.expectEqualStrings;
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
@@ -16,9 +13,70 @@ pub fn main() !void {
     try bw.flush(); // don't forget to flush!
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+test "json simple parse optionals" {
+    const Data = struct { id: u32, name: ?[]u8, email: ?[]u8 };
+    const json_data =
+        \\{ "id": 1, "name": "John Doe", "email": null}
+    ;
+
+    const parsed = try std.json.parseFromSlice(Data, std.testing.allocator, json_data, .{});
+    defer parsed.deinit();
+    const data = parsed.value;
+    try expectEqual(data.id, 1);
+    try expectEqualStrings(data.name.?, "John Doe");
+    try expectEqual(data.email, null);
 }
+
+// test "json optional fields" {
+//     const Data = struct { field1: u32 = 42, field2: ?u32 };
+//     const json_data =
+//         \\{}
+//     ;
+//
+//     const parsed = try std.json.parseFromSlice(Data, std.testing.allocator, json_data, .{});
+//     defer parsed.deinit();
+//     const data = parsed.value;
+//     try expectEqual(data.field1, 42);
+//     try expectEqual(data.field2, null);
+// }
+
+test "json non standard keys" {
+    const Data = struct { @"kebab-case-key": bool, @"space delimited key": bool, @"åäö": bool };
+    const json_data =
+        \\{
+        \\   "kebab-case-key": true,
+        \\   "space delimited key": true,
+        \\   "åäö": true
+        \\}
+    ;
+
+    const parsed = try std.json.parseFromSlice(Data, std.testing.allocator, json_data, .{});
+    defer parsed.deinit();
+    const data = parsed.value;
+    try expect(data.@"kebab-case-key" == true);
+    try expect(data.@"space delimited key" == true);
+    try expect(data.@"åäö" == true);
+}
+
+test "json arrays" {
+    const Data = struct { id: u32, name: ?[]u8, email: ?[]u8 };
+    const json_data =
+        \\{"root": [{ "id": 1, "name": "John Doe", "email": null},
+        \\{ "id": 2, "name": "Jane Doe", "email": null},
+        \\{ "id": 3, "name": "No Dough", "email": "nodough"}]}
+    ;
+    const DataArray = struct {
+        root: []Data,
+    };
+    const parsed = try std.json.parseFromSlice(DataArray, std.testing.allocator, json_data, .{});
+    defer parsed.deinit();
+    const data = parsed.value;
+    // std.debug.print("config.root: {any}\n", .{data.root.?});
+    try (expect(data.root[2].id == 3));
+}
+// test "simple test" {
+//     var list = std.ArrayList(i32).init(std.testing.allocator);
+//     defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+//     try list.append(42);
+//     try std.testing.expectEqual(@as(i32, 42), list.pop());
+// }
